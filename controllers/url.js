4f0d52d0167f0e31fd3508ae14f6b02b36f6d1fa
Aditya Bhave <102917051+adityabhave525@users.noreply.github.com>
2025-02-08 13:19:26 +0530

    Backend of short url completed basic features

--- controllers/url.js
+++ controllers/url.js
@@ -1,25 +1,35 @@
-const shortid = require('shortid');
-const URL = require('../models/url.js');
+const shortid = require("shortid");
+const URL = require("../models/url.js");
 
 async function handleGenerateNewShortURL(req, res) {
-    const body = req.body;
-    if (!body.url) {
-        return res.status(400).json({
-            err: 'Url is required'
-        });
-    }
-    const shortId = shortid();
-    await URL.create({
-        shortId: shortId,
-        redirectUrl: body.url,
-        visitHistory: [],
+  const body = req.body;
+  if (!body.url) {
+    return res.status(400).json({
+      err: "Url is required",
     });
+  }
+  const shortId = shortid();
+  await URL.create({
+    shortId: shortId,
+    redirectUrl: body.url,
+    visitHistory: [],
+  });
 
-    return res.json({
-        id: shortId
-    });
+  return res.json({
+    id: shortId,
+  });
+}
+
+async function handleGetAnalytics(req, res) {
+  const shortId = req.params.shortId;
+  const result = await URL.findOne({ shortId });
+  return res.json({
+    totalClicks: result.visitHistory.length,
+    analytics: result.visitHistory,
+  });
 }
 
 module.exports = {
-    handleGenerateNewShortURL,
+  handleGenerateNewShortURL,
+  handleGetAnalytics
 };
