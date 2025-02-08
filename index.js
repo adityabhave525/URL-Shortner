d25557c543c3558e861ec66c37b3e3a1ac353e9e
Aditya Bhave <102917051+adityabhave525@users.noreply.github.com>
2025-02-08 13:02:03 +0530

    url route and mongo connections

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 
+const urlRoute = require('./routes/url');
+
 const app = express();
 const PORT = 3000;
+
+app.use('/url', urlRoute);
 
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`)
